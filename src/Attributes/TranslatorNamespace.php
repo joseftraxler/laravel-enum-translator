5acdf31e25e3aaf0dfbf0e9be0240282a771b0d1
Josef Traxler <josef.traxler@seznam.cz>
2026-05-12 23:35:45 +0200

    fix: enforce non-empty path in TranslatorNamespace attribute and specify target type

--- src/Attributes/TranslatorNamespace.php
+++ src/Attributes/TranslatorNamespace.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace JosefTraxler\LaravelEnumTranslator\Attributes;
 
-#[\Attribute]
+#[\Attribute(\Attribute::TARGET_CLASS)]
 class TranslatorNamespace
 {
     public function __construct(
         public readonly string $path,
     ) {
+        if ($path === '') {
+            throw new \InvalidArgumentException('Translator namespace path cannot be empty.');
+        }
     }
 }
